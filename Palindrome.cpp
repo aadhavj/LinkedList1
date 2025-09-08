d78ab08a3dec55060973082205228571d10092f9
U-BSD\483830 <483830@SHS-FYBF6X3.beaverton.k12.or.us>
2025-09-08 09:48:13 -0700

    Final palindrome file with comments

--- Palindrome.cpp
+++ Palindrome.cpp
@@ -15,23 +15,27 @@ int main(){
 	int index = 0;
 
 	//Get input possible palindromic string
+	cout << "Enter possible palindrome here: ";
 	cin.get(cstring, 80);
 	cin.get();
 
-	//Filter strings
+	//Filter string
 	for (int i = 0; i < strlen(cstring); i++){
 		if (isalnum(cstring[i])){
 			filteredString[index] = cstring[i];
 			index++;
 		}
 	}
+	filteredString[index] = '\0';
 
 	//Reverse string copy
 	char revString[80];
 	for (int i = 0;i < strlen(filteredString); i++){
 		revString[i] = filteredString[strlen(filteredString)-i-1];
 	}
-
+	revString[index] = '\0';
+	
+	
 	//Lower Cases
 	for (int i = 0; i< strlen(filteredString); i++)
 	{
